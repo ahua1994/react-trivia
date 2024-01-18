df14b4387cc70592eef3312788f817ce6f9cbc98
Andy Hua <user.andy.hua@gmail.com>
2024-01-17 19:40:42 -0800

    dsaf

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,5 +1,5 @@
 const Grid = ({ x, answered, setAnswered, q, current, setCurrent, i }) => {
-    const value = d => (d === "easy" ? 50 : d === "medium" ? 100 : 200);
+    const value = d => (d === "eazchangethis" ? 50 : d === "medium" ? 100 : 200);
     return (
         <button key={x.id} onClick={() => setCurrent(i)} disabled={answered.includes(x.id)}>
             {x.question.text}
